87ba4ca2a421e57dae17bf882201a059d5b3422c
asthmo <codemachine@mail.ru>
2023-10-16 12:45:22 +0300

    commit

---
+++ Locators/kinopoisk_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class KinopoiskPageLocators:
+    INPUT_FILM_TITLE = (By.ID, 'find_film')
+    SELECT_COUNTRY = (By.ID, 'country')
+    SELECT_GENRE = (By.ID, 'm_act[genre]')
+    SUBMIT = (By.CLASS_NAME, 'nice_button')
+
+    FILM_TITLE = (By.CLASS_NAME, 'name')
---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+import allure
+import json
+from selenium import webdriver
+
+@pytest.fixture(scope='session')
+def config():
+    with open('settings.json', 'r') as f:
+        settings = json.loads(f.read())
+    return settings
+
+
+@pytest.fixture(scope='session')
+def driver(config):
+    if config['browser'].lower() == 'chrome':
+        driver = webdriver.Chrome()
+    elif config['browser'].lower() == 'firefox':
+        driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
